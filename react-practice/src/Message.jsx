13d50a2fc74483285748d63ac9d4c9126f6558e1
Akanimoh12 <akanimohjohnson2003@gmail.com>
2024-11-27 21:39:53 +0100

    my assignment on useState

---
+++ react-practice/src/Message.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import ReactDOM from "react-dom/client";
+
+function CarName() {
+  const [car, setCar] = useState("4Runner");
+
+  return (
+    <>
+      <h1>My best car is {car}!</h1>
+      <button
+        type="button"
+        onClick={() => setCar("Volvo")}
+      >Volvo</button>
+    </>
+  )
+}
+
+export default CarName;
+
+function Message() {
+    const name = "Johnson"
+    const age = 30;
+    return <h1>Hello {name + age}</h1>
+}
+
+// export default Message;
